f3b31f73f031bf8739dec8067aec842276b4e24e
Temitope Adebiyi <adebiyitope26@gmail.com>
2021-06-19 10:39:56 +0100

    tabor view created

--- moviePage/ViewController.swift
+++ moviePage/ViewController.swift
@@ -7,14 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+//        UITabBar.appearance().barTintColor = .systemBackground
+//        tabBar.tintColor = .label
         // Do any additional setup after loading the view.
+        setupVCs()
+    }
+    
+    func setupVCs() {
+        viewControllers = [
+            createNavController(for: HomeViewController(), title: NSLocalizedString("Home", comment: ""), image: UIImage(systemName: "house")!),
+            createNavController(for: FavouriteViewController(), title: NSLocalizedString("Favorite", comment: ""), image: UIImage(systemName: "bookmark")!)
+                                
+        ]
     }
 
-
+    private func createNavController(for rootViewController: UIViewController, title: String, image:UIImage) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootViewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = image
+        navController.navigationBar.prefersLargeTitles = true
+        rootViewController.navigationItem.title = title
+        return navController
+    }
 }
 
